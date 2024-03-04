a205562409add2a6d34a1f272439b5c79749d8aa
dbbnicole <dbbnicole>
2024-03-04 20:40:16 +0000

    adsf

--- 00_context.py
+++ 00_context.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+# MAGIC %md asdf
+
+# COMMAND ----------
+
 # MAGIC %md 
 # MAGIC You may find this series of notebooks at https://github.com/databricks-industry-solutions/toxicity-detection-in-gaming and more information about this solution accelerator at https://www.databricks.com/solutions/accelerators/toxicity-detection-for-gaming.
 
@@ -8,12 +12,12 @@
 # MAGIC <div >
 # MAGIC   <img src="https://cme-solution-accelerators-images.s3-us-west-2.amazonaws.com/toxicity/solution-accelerator-logo.png"; width="50%">
 # MAGIC </div>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Overview
-# MAGIC 
+# MAGIC
 # MAGIC Toxicity can have a large impact on player engagement and satisfaction. Game companies are working on ways to address forms of toxicity in their platforms. One of the most common interactions with toxicity is in chat boxes or in-game messaging systems. As companies are becoming more data driven, the opportunity to detect toxicity using the data at hand is present, but technically challenging. This solution accelerator is a head start on deploying a ML-enhanced data pipeline to address toxic messages in real time.
-# MAGIC 
+# MAGIC
 # MAGIC ** Authors**
 # MAGIC - Duncan Davis [<duncan.davis@databricks.com>]
 # MAGIC - Dan Morris [<dan.morris@databricks.com>]
@@ -23,7 +27,7 @@
 
 # MAGIC %md
 # MAGIC Copyright Databricks, Inc. [2022]. The source in this notebook is provided subject to the [Databricks License](https://databricks.com/db-license-source).  All included or referenced third party libraries are subject to the licenses set forth below.
-# MAGIC 
+# MAGIC
 # MAGIC |Library Name|Library license | Library License URL | Library Source URL |
 # MAGIC |---|---|---|---|
 # MAGIC |Spark-nlp|Apache-2.0 License| https://nlp.johnsnowlabs.com/license.html | https://www.johnsnowlabs.com/
